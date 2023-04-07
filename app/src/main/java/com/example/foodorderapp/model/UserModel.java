322c4dd33465d6ee0bcb17b5f2c39d05edee84eb
KemVaniNe <huynhthihongvan2703@gmail.com>
2023-04-07 09:54:21 +0700

    Van

--- app/src/main/java/com/example/foodorderapp/model/UserModel.java
+++ app/src/main/java/com/example/foodorderapp/model/UserModel.java
@@ -5,6 +5,9 @@ public class UserModel {
     private String name;
     private String pass;
 
+    private String email;
+    private String phone;
+
     public UserModel(){
 
     }
@@ -12,6 +15,13 @@ public class UserModel {
     public UserModel(String name, String pass) {
         this.name = name;
         this.pass = pass;
+    }
+
+    public UserModel(String name, String pass, String email, String phone) {
+        this.name = name;
+        this.pass = pass;
+        this.email = email;
+        this.phone = phone;
     }
 
     public String getName() {
@@ -28,5 +38,21 @@ public class UserModel {
 
     public void setPass(String pass) {
         this.pass = pass;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
     }
 }
